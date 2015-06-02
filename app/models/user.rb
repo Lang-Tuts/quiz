e43fbfebcea28a2c5065dda58bffa7c45d90d322
Rakesh.P.D <errakeshpd@gmail.com>
2015-06-02 06:19:39 +0530

    database modified

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  scope :online, lambda{ where("updated_at > ?", 10.minutes.ago) }
+  def online?
+        updated_at > 10.minutes.ago
+  end
 end
