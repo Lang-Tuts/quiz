8e34d68de943d786df041abe8522e402dafe5677
Rakesh.P.D <errakeshpd@gmail.com>
2015-06-02 08:18:23 +0530

    attendexam window modified

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150601173517) do
+ActiveRecord::Schema.define(version: 20150602021005) do
 
   create_table "answers", force: :cascade do |t|
     t.integer  "question_id"
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 20150601173517) do
     t.integer  "actual_answer"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+  end
+
+  create_table "attendexams", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "exam_id"
+    t.integer  "question_id"
+    t.integer  "answer_id"
+    t.integer  "mark"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "exams", force: :cascade do |t|
