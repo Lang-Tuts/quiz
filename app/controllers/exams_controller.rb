0a34221a926f6210508170f2c1492e1b09db47fe
Rakesh.P.D <errakeshpd@gmail.com>
2015-06-03 07:39:16 +0530

    modified exam with exam qustion count

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -69,6 +69,6 @@ class ExamsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def exam_params
-      params.require(:exam).permit(:name)
+      params.require(:exam).permit(:name,:count)
     end
 end
